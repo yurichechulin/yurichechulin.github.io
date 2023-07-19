6301c6844ce77bb9da2ba62de871352346695912
Yuri Chechulin <yu.s.chechulin@gmail.com>
2023-07-19 18:39:39 +0500

    Update geo.js

--- geo.js
+++ geo.js
@@ -1,37 +1,38 @@
 function sendRequest(publicKey = false) {
-  var publicKey = '6LcWXc8gAAAAAMpgB0-7TzTELlr8f7T2XiTrexO5';
-  getRecaptchaToken(publicKey, makeAjaxCall1, errorCapture);
+  // var publicKey = '6LcWXc8gAAAAAMpgB0-7TzTELlr8f7T2XiTrexO5';
+  // getRecaptchaToken(publicKey, makeAjaxCall1, errorCapture);
+	console.log('asdasdasdasdadadasda')
 }
 
 
-function makeAjaxCall1(token) {
-  // var headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' };
-  // var url = 'https://rp.uremont.dev/result_rsa.php';
-  // var data = '&gRecaptchaResponse=' + token;
-  // fetch(url, {
-  //   method: 'POST',
-  //   headers: headers,
-  //   body: data
-  // });
+// function makeAjaxCall1(token) {
+//   // var headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' };
+//   // var url = 'https://rp.uremont.dev/result_rsa.php';
+//   // var data = '&gRecaptchaResponse=' + token;
+//   // fetch(url, {
+//   //   method: 'POST',
+//   //   headers: headers,
+//   //   body: data
+//   // });
 
 
-  // window.clearInterval(window.interval_submit_ajax);
-  // window.interval_submit_ajax = window.setInterval(sendRequest, 60000);
-  // //grecaptcha.reset();
-  // //location.reload(true);
+//   // window.clearInterval(window.interval_submit_ajax);
+//   // window.interval_submit_ajax = window.setInterval(sendRequest, 60000);
+//   // //grecaptcha.reset();
+//   // //location.reload(true);
 
-   console.log(token);
+//    console.log(token);
 	
-  return false;
-}
+//   return false;
+// }
 
 
 
-function errorCapture(err) {
-  return false;
-}
+// function errorCapture(err) {
+//   return false;
+// }
 
 
-function reload() {
-  location.reload(true);
-}
+// function reload() {
+//   location.reload(true);
+// }
