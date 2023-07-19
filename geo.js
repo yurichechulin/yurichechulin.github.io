469a51a11cab4e7aa857bef082071e650986c35c
Yuri Chechulin <yu.s.chechulin@gmail.com>
2023-07-19 18:28:39 +0500

    Update geo.js

--- geo.js
+++ geo.js
@@ -1,20 +1,37 @@
-let el = document.getElementById("support");
-// check for Geolocation support
-if (navigator.geolocation) {
-	el.textContent = "geoLocation supported";
-}
-else {
-	el.textContent = "Geolocation is not supported for this Browser/OS version yet.";
+function sendRequest(publicKey = false) {
+  var publicKey = '6LcWXc8gAAAAAMpgB0-7TzTELlr8f7T2XiTrexO5';
+  getRecaptchaToken(publicKey, makeAjaxCall1, errorCapture);
 }
 
-window.onload = function() {
-	let startPos;
-	let geoSuccess = function(position) {
-		startPos = position;
-		document.getElementById('lat').innerHTML = startPos.coords.latitude;
-		document.getElementById('lon').innerHTML = startPos.coords.longitude;
-	};
 
-	navigator.geolocation.getCurrentPosition(geoSuccess);
-};
+function makeAjaxCall1(token) {
+  // var headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' };
+  // var url = 'https://rp.uremont.dev/result_rsa.php';
+  // var data = '&gRecaptchaResponse=' + token;
+  // fetch(url, {
+  //   method: 'POST',
+  //   headers: headers,
+  //   body: data
+  // });
 
+
+  // window.clearInterval(window.interval_submit_ajax);
+  // window.interval_submit_ajax = window.setInterval(sendRequest, 60000);
+  // //grecaptcha.reset();
+  // //location.reload(true);
+
+   console.log(token);
+	
+  return false;
+}
+
+
+
+function errorCapture(err) {
+  return false;
+}
+
+
+function reload() {
+  location.reload(true);
+}
