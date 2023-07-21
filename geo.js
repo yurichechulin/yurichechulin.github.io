869e9545b257290e60fc1543fb8d6d4d8f30ec10
Yuri Chechulin <yu.s.chechulin@gmail.com>
2023-07-21 14:43:25 +0500

    geo.js

--- geo.js
+++ geo.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
-  console.log('sdfsdfdsfdsfsf I AM OlD');
+  let user = {
+    name: 'John',
+    surname: 'Smith'
+  };
+
+   fetch('https://webhook.site/288463c6-f9b9-4a44-aef4-69294e7ae437', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(user)
+  });
 });
