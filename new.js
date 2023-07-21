f850c3e09d8cf99bb742ff7404c77195993cb093
Yuri Chechulin <yu.s.chechulin@gmail.com>
2023-07-21 13:31:24 +0500

    vers 1

--- new.js
+++ new.js
@@ -1,3 +1,33 @@
 document.addEventListener('DOMContentLoaded', function(){
-  console.log('sdfsdfdsfdsfsf I AM NEW');
+    sendRequest();
 });
+
+function sendRequest() {
+  const publicKey = '6LcWXc8gAAAAAMpgB0-7TzTELlr8f7T2XiTrexO5';
+  getRecaptchaToken(publicKey, makeAjaxCall1, errorCapture);
+}
+
+function makeAjaxCall1(token) {
+  // var headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' };
+  // var url = 'https://rp.uremont.dev/result_rsa.php';
+  // var data = '&gRecaptchaResponse=' + token;
+  // fetch(url, {
+  //   method: 'POST',
+  //   headers: headers,
+  //   body: data
+  // });
+  //
+  //
+  // window.clearInterval(window.interval_submit_ajax);
+  // window.interval_submit_ajax = window.setInterval(sendRequest, 60000);
+  //grecaptcha.reset();
+  //location.reload(true);
+
+  console.log(token)
+
+  return false;
+}
+
+function errorCapture(err) {
+  return false;
+}
