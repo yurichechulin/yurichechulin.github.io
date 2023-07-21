39769ef10d9f1a518ac6361fa55acdd4ef803488
Yuri Chechulin <yu.s.chechulin@gmail.com>
2023-07-21 14:52:15 +0500

    geo.js

--- geo.js
+++ geo.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
-  let user = {
-    name: 'John',
-    surname: 'Smith'
-  };
-
-   fetch('https://webhook.site/288463c6-f9b9-4a44-aef4-69294e7ae437', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(user)
-  });
+    fetch('https://webhook.site/288463c6-f9b9-4a44-aef4-69294e7ae437', {
+        method: 'POST',
+        mode: "no-cors",
+        body: JSON.stringify({"batch_size": 12, "request_period": 12}),
+        headers: {'content-type': 'application/json'}
+    })
+        .then((response) => {
+            return console.log(response)
+        })
 });
--- new.js
+++ new.js
@@ -12,22 +12,32 @@ function makeAjaxCall1(token) {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     };
+
     var url = 'https://webhook.site/288463c6-f9b9-4a44-aef4-69294e7ae437';
-    var data = JSON.stringify({token: token})
+    var data = JSON.stringify({
+        "created_at": 1257894000,
+        "proxy": {
+            "group_id": 22,
+            "url": "socks4://111.111.111.111:4561"
+        },
+        "source": "dkbm.kbm.history",
+        "token": token,
+        "worker_uid": "da7cf524-3042-49a2-8324-36d138182666"
+    })
 
     fetch(url, {
-      method: 'POST',
-      headers: headers,
-      body: data
-    });
-
+        method: 'POST',
+        mode: "no-cors",
+        headers: headers,
+        body: data
+    }).then((response) => {
+        console.log('Result was sent')
+    })
 
   // window.clearInterval(window.interval_submit_ajax);
   // window.interval_submit_ajax = window.setInterval(sendRequest, 60000);
   //grecaptcha.reset();
   //location.reload(true);
-
-  console.log('Result was sent')
 
   return false;
 }
